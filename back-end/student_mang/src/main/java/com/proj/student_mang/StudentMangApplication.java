83fcc7cf861328fc31f281da1fcbd0fe6e0ed010
abdelouahedGit <abdeouahedfanan@gmail.com>
2023-02-21 15:28:15 +0100

    myChange

--- back-end/student_mang/src/main/java/com/proj/student_mang/StudentMangApplication.java
+++ back-end/student_mang/src/main/java/com/proj/student_mang/StudentMangApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentMangApplication {
-
+  int s=12;
     public static void main(String[] args) {
         SpringApplication.run(StudentMangApplication.class, args);
     }
