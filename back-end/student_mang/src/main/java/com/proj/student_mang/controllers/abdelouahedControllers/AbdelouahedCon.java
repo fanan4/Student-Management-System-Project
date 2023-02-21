d749f79a7a4378e22a66dd4d55ad1310d16ac0c0
abdelouahedGit <abdeouahedfanan@gmail.com>
2023-02-21 17:00:05 +0100

    security

--- back-end/student_mang/src/main/java/com/proj/student_mang/controllers/abdelouahedControllers/AbdelouahedCon.java
+++ back-end/student_mang/src/main/java/com/proj/student_mang/controllers/abdelouahedControllers/AbdelouahedCon.java
@@ -1,4 +1,25 @@
 package com.proj.student_mang.controllers.abdelouahedControllers;
 
+
+import com.proj.student_mang.custoprepos.usersRepo;
+import com.proj.student_mang.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AbdelouahedCon {
+    @Autowired
+    usersRepo  repo;
+    @GetMapping("/getUsers")
+    List<User> getUsers(){ return repo.findAll() ;}
+    @PutMapping("/addUser")
+    User addUser(@RequestBody User user){
+       return repo.save(user);
+    }
+
 }
--- back-end/student_mang/src/main/java/com/proj/student_mang/custoprepos/usersRepo.java
+++ back-end/student_mang/src/main/java/com/proj/student_mang/custoprepos/usersRepo.java
@@ -1,8 +1,15 @@
 package com.proj.student_mang.custoprepos;
 
 import com.proj.student_mang.models.User;
+import org.springframework.data.domain.Example;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface usersRepo extends MongoRepository<User , String> {
+import java.util.List;
 
+public interface usersRepo extends MongoRepository<User , String> {
+    @Override
+    List<User> findAll();
+
+    @Override
+    <S extends User> S save(S entity);
 }
