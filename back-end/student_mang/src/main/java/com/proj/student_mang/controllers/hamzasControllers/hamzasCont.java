18f760c8fe601aee4b88f191eb3bec63b549d93e
hamza_from_mint <hamzadouai7@gmail.com>
2023-02-21 17:00:50 +0100

    add repos , Users object , controllers folder

--- back-end/student_mang/src/main/java/com/proj/student_mang/controllers/hamzasControllers/hamzasCont.java
+++ back-end/student_mang/src/main/java/com/proj/student_mang/controllers/hamzasControllers/hamzasCont.java
@@ -1,4 +1,27 @@
 package com.proj.student_mang.controllers.hamzasControllers;
 
+import com.proj.student_mang.custoprepos.usersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+
+@RestController
+
 public class hamzasCont {
+
+    private usersRepo repo ;
+    @Autowired
+    public hamzasCont(usersRepo repo){
+        this.repo = repo ;
+    }
+                @DeleteMapping("/deleteusre/{id}")
+                @ResponseStatus
+        public void deleteUser(@RequestParam String id){
+try{
+    repo.deleteById(id);
+}catch (Exception e){
+    e.printStackTrace();
+}
+    }
 }
