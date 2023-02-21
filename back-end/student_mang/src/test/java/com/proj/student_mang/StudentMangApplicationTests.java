35806c7221646765ed18cbbc3e9bc370738591d9
abdelouahedGit <abdeouahedfanan@gmail.com>
2023-02-21 14:36:24 +0100

    init

---
+++ back-end/student_mang/src/test/java/com/proj/student_mang/StudentMangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.student_mang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
